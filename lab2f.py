7fae2cd71403983a0127dc1f8e4061431f409957
Ched Duloy <185662210-myseneca>
2024-05-23 16:58:16 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#Author: Ched Duloy
+#Author ID: 185662210
+#Date Created: 2024/05/23
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+	print(timer)
+	timer -= 1
+
+print("blast off!")
